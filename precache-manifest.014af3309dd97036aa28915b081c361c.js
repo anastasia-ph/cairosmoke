e23e5b15013be798d710ada93402ade3eaef07f3
Anastasia <uranusinfection@gmail.com>
2023-05-02 22:56:02 +0300

    Updates

---
+++ precache-manifest.014af3309dd97036aa28915b081c361c.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "c433a51f703b6f308c4f",
+    "url": "/cairosmoke/static/css/main.1996d86c.chunk.css"
+  },
+  {
+    "revision": "c433a51f703b6f308c4f",
+    "url": "/cairosmoke/static/js/main.c433a51f.chunk.js"
+  },
+  {
+    "revision": "6f6f4986655de22842af",
+    "url": "/cairosmoke/static/js/1.6f6f4986.chunk.js"
+  },
+  {
+    "revision": "327f9ec1708fccdefb32",
+    "url": "/cairosmoke/static/js/2.327f9ec1.chunk.js"
+  },
+  {
+    "revision": "7afdc70609e1424b397c",
+    "url": "/cairosmoke/static/js/runtime~main.7afdc706.js"
+  },
+  {
+    "revision": "b55b30c4760000ec7b92a8a4dad81a2c",
+    "url": "/cairosmoke/static/media/UnifrakturMaguntia-Regular.b55b30c4.ttf"
+  },
+  {
+    "revision": "946b94bad382c98a13e0efb9be995a26",
+    "url": "/cairosmoke/static/media/IbarraRealNova-Italic.946b94ba.ttf"
+  },
+  {
+    "revision": "c9fed3d8183f5953deaeab0b63cd7f1a",
+    "url": "/cairosmoke/static/media/IbarraRealNova-Bold.c9fed3d8.ttf"
+  },
+  {
+    "revision": "127e729d49d9137a8d376ff3be9ce21a",
+    "url": "/cairosmoke/static/media/lightningBackground.127e729d.jpg"
+  },
+  {
+    "revision": "a7fd1e90f70f4d026df89b8411e84c12",
+    "url": "/cairosmoke/index.html"
+  }
+];
