092804f105ec2e815a9c68316c77a7b26114a085
Anastasia <uranusinfection@gmail.com>
2023-05-20 23:48:44 +0300

    changed the path to animation

--- src/components/components_main-page/cairoEffectLightning.jsx
+++ src/components/components_main-page/cairoEffectLightning.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const CairoEffectLightning = () => {
     return (
         <div className="cairo__container">
-            <img class="cairo__lightning" src="../assets/lightningAnimation.png"></img>
+            <img class="cairo__lightning" src="assets/lightningAnimation.png"></img>
         </div>
     )
 }
